76007046d6459d148c38544ecc67195ef1a172e3
larissaslff <110000521+larissaslff@users.noreply.github.com>
2023-03-02 21:19:05 -0300

    cria AutorDTO

---
+++ biblioteca/src/main/java/com/biblioteca/biblioteca/modelo/AutorDTO.java
@@ -0,0 +1,17 @@
+package com.biblioteca.biblioteca.modelo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AutorDTO {
+
+    private String firstName;
+
+    private String lastName;
+}
