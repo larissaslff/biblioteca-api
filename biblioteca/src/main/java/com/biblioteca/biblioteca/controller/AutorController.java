364310556d965087ae03bb5faf8cfcfb510d9a00
larissaslff <110000521+larissaslff@users.noreply.github.com>
2023-03-14 19:54:46 -0300

    add findAll

--- biblioteca/src/main/java/com/biblioteca/biblioteca/controller/AutorController.java
+++ biblioteca/src/main/java/com/biblioteca/biblioteca/controller/AutorController.java
@@ -1,10 +1,14 @@
 package com.biblioteca.biblioteca.controller;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,10 +30,21 @@ public class AutorController {
     private ModelMapper mapper;
 
     @PostMapping
-    public ResponseEntity<AutorDTO> create(@RequestBody AutorDTO autorDTO, UriComponentsBuilder uriBuilder) {
+    public ResponseEntity<AutorDTO> createAutor(@RequestBody AutorDTO autorDTO, UriComponentsBuilder uriBuilder) {
         Autor autor = repository.save(mapper.map(autorDTO, Autor.class));
         URI uri = uriBuilder.path("/autores/{id}").buildAndExpand(autor.getId()).toUri();
         return ResponseEntity.created(uri).body(autorDTO);
 
     }
+
+    @GetMapping
+    public ResponseEntity<List<AutorDTO>> findAllAutores(){
+        List<Autor> autores = repository.findAll();
+        List<AutorDTO> retorno = new ArrayList<>();
+        for (Autor autor : autores) {
+            retorno.add(mapper.map(autor, AutorDTO.class));
+        }
+        return ResponseEntity.ok().body(retorno);
+    }
+
 }
